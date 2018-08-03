213f311fd2d5336f9a291d436e8b228d8a695f1b
odifmonster <lamanwyner@icloud.com>
2018-08-03 00:16:55 -0400

    updating features...

--- update_features.sh
+++ update_features.sh
@@ -2,7 +2,7 @@
 
 cd ~/Downloads
 rm ~/Desktop/BWSI/Final_Project/Data/vector_data.csv
-mv extracted_data.pkl ~/Desktop/BWSI/Final_Project/Data/
+mv vector_data.csv ~/Desktop/BWSI/Final_Project/Data/
 cd ~/Desktop/BWSI/Final_Project
 git add .
 git commit -m "updating features..."
